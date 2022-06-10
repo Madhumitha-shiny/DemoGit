de3c246d0205abf77cc38b70ccb9dcb984017ad4
MadhumithaShiny <smshiny.12@gmail.com>
2022-06-10 22:31:05 +0530

    Deployed my react new  app

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ const Header = ({img,text}) => {
     <div align="center" className="HeadSection">
         <img src={img} alt="logo" height={25} width={75} />
         <h2 className="heading">{text}</h2>
-        <p style={{fontSize:'18px'}}>to continue to Gmail</p>
+        <p style={{fontSize:'18px'}}>to continue to Gmail okay</p>
     </div>
   )
 }
